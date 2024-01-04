017e5c9abd7f61c74ae3057cc1e3dfb3f2ebcc6e
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-04 21:07:25 +0600

    Update bank.html and deposit.js

--- deposit.js
+++ deposit.js
@@ -3,13 +3,37 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
    //step-2: get the deposit amount from the input field
    // foor input field we have to use .value
     const depositInput = document.getElementById('deposit-amount');
-    const newDepositAmount = depositInput.value;
-    console.log(newDepositAmount);
-
+    const newDepositAmountString = depositInput.value;
+    // convert string to float
+    const newDepositAmount = parseFloat(newDepositAmountString);
+   
+    
     // step-3: update current deposit total
     // for non-input field we have to use .innerText
     const depositTotal = document.getElementById('deposit-total');
-    const previousDepositAmount = depositTotal.innerText;
-    depositTotal.innerText = newDepositAmount
+    const previousDepositAmountString = depositTotal.innerText;
+    // convert string to float
+    const previousDepositAmount = parseFloat(previousDepositAmountString);
+
+    // step-4: update current deposit total
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    // update deposit total
+     depositTotal.innerText = newDepositTotal;
+
+    // step-5: update balance current total balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotal.innerText;
+    // convert string to float
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+      // step-6: update balance total
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // update balance total
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
+
+    // step-7: clear the deposit input field
+     depositInput.value = '';
 
 });
---
+++ withdraw.js
@@ -0,0 +1,53 @@
+/*
+   1. add event listener to the withdraw button
+    2. get the withdraw amount from the input field
+    2-5. also make sure to convart the input a number by using parseFloat()
+    3. update current withdraw total
+    4. celculate the total withdraw amount
+    4-5. set total withrow amoount
+    5. get the previous balance total
+    6. celculate the new balance total
+    6-5. set the new balance total
+    7. clear the withdraw input field
+
+*/
+
+
+
+
+//step-1: add event listener to the withdraw button
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    //step-2
+    const withdrawInput = document.getElementById('withdraw-amount');
+    const withdrawAmountString = withdrawInput.value;
+    // convert string to float
+    const withdrawAmount = parseFloat(withdrawAmountString);
+    // step-3
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotal.innerText;
+    // convert string to float
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+   
+    // step-4
+    const newWithdrawTotal = previousWithdrawTotal + withdrawAmount;
+    // update withdraw total amount
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // step-5
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotal.innerText;
+    // convert string to float
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    
+    // step-6
+    const newBalanceTotal = previousBalanceTotal - withdrawAmount;
+    // update balance total amount
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
+  // step-7
+    withdrawInput.value = '';
+
+
+});

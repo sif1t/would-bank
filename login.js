f5bbdecbf76435e92e21ce53b50b7481e483caa5
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-04 14:57:40 +0600

    complitr login page

---
+++ login.js
@@ -0,0 +1,20 @@
+// step-1: get the button element
+document.getElementById('btn-submit').addEventListener('click', function (){
+      // step-2: get user email
+      // always remember to use. value to get the value of the input field
+        const emailField = document.getElementById('user-email');
+        const userEmail = emailField.value;
+        console.log (userEmail);
+        // step-3: get user password
+        const passwordField = document.getElementById('user-password');
+        const userPassword = passwordField.value;
+        console.log (userEmail, userPassword);
+        //Danger: DO NOT VERIFY email and password on the client side
+        // step-4: check email and password valid user or not
+        if (userEmail == 'sifat@gmail.com' && userPassword == 'sifat22'){
+            console.log('valid user');
+        }
+        else{
+            console.log('invalid user');
+        }
+});
